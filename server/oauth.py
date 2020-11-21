fc01c6af5705728c4b4f840cca69a6de4a7bd1b3
Raphaël Margueron <raphael.margueron@gmail.com>
2020-11-21 14:04:39 +0100

    add oauth simple connexion code to github

---
+++ server/oauth.py
@@ -0,0 +1,37 @@
+from github import Github
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+
+def get_github(code):
+    # get access token
+    url = "https://github.com/login/oauth/access_token"
+    data = {
+        "client_id": os.getenv("GITHUB_CLIENT_ID"),
+        "client_secret": os.getenv("GITHUB_CLIENT_SECRET"),
+        "code": code,
+
+    }
+    headers = {
+        "Accept": "application/json"
+    }
+    r = requests.post(url, data=data, headers=headers)
+    answer = r.json()
+    access_token = answer["access_token"]
+    g = Github(access_token)
+    return g
+
+
+def get_github_auth_url():
+    return f"https://github.com/login/oauth/authorize?client_id={os.getenv('GITHUB_CLIENT_ID')}"
+
+
+if __name__ == '__main__':
+    print(get_github_auth_url())
+    code = input("code:")
+    g = get_github(code)
+    u = g.get_user()
+    print(u.id, u.avatar_url)
