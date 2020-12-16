7bf51af0f24898afc7496b61b308b379329a0152
Ludovic Herbelin <ludovic.herbelin@gmail.com>
2020-12-16 11:46:30 +0100

    Updates on the dockerfile to build before copying scripts. Fixed websockets connection error by adding delay before starting thingy.py

--- server/run_server.sh
+++ server/run_server.sh
@@ -2,5 +2,7 @@
 
 #set -e
 
-exec python3.7 ./server.py &
-exec python3.7 ./thingy.py
+exec python3.7 ./server.py & 
+# add delay so the server can create the websockets etc.
+sleep 5
+exec python3.7 ./thingy.py 
